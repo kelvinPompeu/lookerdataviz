6c695bc357d74bb3fb5bf49336466b9bc419d6a8
Kelvin Pompeu <pompeuk@google.com>
2025-03-24 13:24:14 +0000

    gf

--- visualizations/columncustom.js
+++ visualizations/columncustom.js
@@ -94,7 +94,8 @@ looker.plugins.visualizations.add({
         labels: {
           useHTML: true,
           animate: true,
-          format: '{chart.options.countries.(value).ucCode}<br><img style='display:inline-block;height:32px;' src='https://www.worldometers.info//img/flags/small/tn_{value}-flag.gif'
+          format: "{chart.options.countries.(value).ucCode}<br>
+          <img style='display:inline-block;height:32px;' src='https://www.worldometers.info//img/flags/small/tn_{value}-flag.gif"
           style: {
             textAlign: 'center'
           }
