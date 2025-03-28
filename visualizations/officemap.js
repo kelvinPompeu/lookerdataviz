c126efe1f936a247f3cef7da8d34b71047388613
Kelvin Pompeu <pompeuk@google.com>
2025-03-28 21:30:53 +0000

    erfer

--- visualizations/officemap.js
+++ visualizations/officemap.js
@@ -29,16 +29,10 @@ looker.plugins.visualizations.add({
 
     // Processar os dados do Looker para o formato do Highcharts
     const seriesData = data.map(row => {
-
-    console.log("row[dimensionFieldX]", row[dimensionFieldX])
-    console.log("row[dimensionFieldY]", row[dimensionFieldY])
-    console.log("row[measureField]", row[measureField])
-
       return {
-        x: 4,
-        y: 2,
-        value: 100,
-
+        x: row[dimensionFieldX],
+        y: row[dimensionFieldY],
+        value: row[measureField]
       };
     });
 
@@ -76,18 +70,7 @@ looker.plugins.visualizations.add({
       },
       series: [{
         name: 'Intensidade',
-        data: [
-            [0, 0, 10], [0, 1, 19], [0, 2, 8], [0, 3, 24], [0, 4, 67],
-            [1, 0, 156], [1, 1, 154], [1, 2, 78], [1, 3, 117], [1, 4, 48],
-            [2, 0, 160], [2, 1, 149], [2, 2, 123], [2, 3, 64], [2, 4, 52],
-            [3, 0, 72], [3, 1, 56], [3, 2, 114], [3, 3, 19], [3, 4, 16],
-            [4, 0, 38], [4, 1, 5], [4, 2, 8], [4, 3, 117], [4, 4, 115],
-            [5, 0, 88], [5, 1, 32], [5, 2, 12], [5, 3, 6], [5, 4, 120],
-            [6, 0, 13], [6, 1, 44], [6, 2, 88], [6, 3, 98], [6, 4, 96],
-            [7, 0, 31], [7, 1, 1], [7, 2, 82], [7, 3, 32], [7, 4, 30],
-            [8, 0, 85], [8, 1, 97], [8, 2, 123], [8, 3, 64], [8, 4, 84],
-            [9, 0, 47], [9, 1, 114], [9, 2, 31], [9, 3, 48], [9, 4, 91]
-        ],
+        data: seriesData,
         borderWidth: 0, // Sem bordas nas células
         nullColor: 'rgba(0,0,0,0)', // Células sem dados transparentes
         interpolation: true // Sem interpolação entre as células
