909c3fe587905c4f472a0392f6d91262cf6779d6
Kelvin Pompeu <pompeuk@google.com>
2025-03-28 23:44:26 +0000

    gfg

--- visualizations/officemap.js
+++ visualizations/officemap.js
@@ -86,7 +86,7 @@ looker.plugins.visualizations.add({
           },
           chartOptions: {
             chart: {
-              height: 260 // Ajuste conforme necessário
+              height: 350 // Ajuste conforme necessário
             }
           }
         }]
